47d3259e184ed360330cb4af50b4cb1afa07da8a
premchand11 <pankupremchand@gmail.com>
2025-02-15 01:33:06 +0530

    new

---
+++ app.js
@@ -0,0 +1,8 @@
+const heading1 = React.createElement("h1", { id: "title" }, "heading1");
+const heading2 = React.createElement("h2", { id: "title" }, "heading2");
+const div = React.createElement("div", { id: "container" }, [
+  heading1,
+  heading2,
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(div);
