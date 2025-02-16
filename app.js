9c3abdec3b61654efa0cd5d7e54c67693ce269c4
premchand11 <pankupremchand@gmail.com>
2025-02-16 23:15:04 +0530

    class4

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ const RestroCard = (props) => {
     <div className="card">
       <img src={props.restaurant.image}></img>
       <h2>{props.restaurant.name}</h2>
-      <h3>{props.restaurant.cuisines.join(",")}</h3>
+      <h3>{props.restaurant.data?.cuisines.join(",")}</h3>
       <h4>{props.restaurant.rating} stars</h4>
     </div>
   );
